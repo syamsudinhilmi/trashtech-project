fa87936b0d2678a50af700496af43a35014c0f91
Hilmi Syamsudin <hilmisyamsudin@students.unnes.ac.id>
2023-11-29 16:44:46 +0700

    Penyesuaian Signin UI

---
+++ app/src/main/java/com/playdeadrespawn/trashtech/ui/activity/SigninActivity.kt
@@ -0,0 +1,12 @@
+package com.playdeadrespawn.trashtech.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.playdeadrespawn.trashtech.R
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+    }
+}
