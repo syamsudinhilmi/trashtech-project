0ac1d94750379785cb66726bc267e401e83c0524
Hilmi Syamsudin <hilmisyamsudin@students.unnes.ac.id>
2023-12-02 16:55:29 +0700

    Fixing BuildConfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,12 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 }
 
 dependencies {
-    val cameraxVersion = "1.3.0"
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -50,10 +49,6 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.camera:camera-camera2:${cameraxVersion}")
-    implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
-    implementation("androidx.camera:camera-view:${cameraxVersion}")
 
     implementation("de.hdodenhof:circleimageview:3.1.0")
 }
--- app/src/main/java/com/playdeadrespawn/trashtech/Utils.kt
+++ app/src/main/java/com/playdeadrespawn/trashtech/Utils.kt
@@ -7,7 +7,6 @@ import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import androidx.core.content.FileProvider
-import de.hdodenhof.circleimageview.BuildConfig
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -38,7 +37,7 @@ private fun getImageUriForPreQ(context: Context): Uri {
     if (imageFile.parentFile?.exists() == false) imageFile.parentFile?.mkdir()
     return FileProvider.getUriForFile(
         context,
-        "com.playdeadrespawn.trashtech.fileprovider",
+        "${BuildConfig.APPLICATION_ID}.fileprovider",
         imageFile
     )
 }
